86e42a9aeedf3ac5e02a9d5c5271f49c55602f1c
stolenhead <andrea.palomino.ruby@gmail.com>
2018-01-23 21:56:48 -0500

    cambio html para el sitio

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'p^9f$$h1%abbb^i0+zck0x-xw3@#n8i(9!wspbx85#%c3#fksr'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
